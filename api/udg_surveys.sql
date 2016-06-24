93583e2a9ab8e46fb4a61a2de7ff4c0b46e639c6
erosespinola <eros.espinola.gonzalez@gmail.com>
2016-06-24 02:10:29 -0500

    new db schema

--- api/udg_surveys.sql
+++ api/udg_surveys.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jun 23, 2016 at 03:11
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2016 at 09:09 AM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `event_types`
 --
 
-CREATE TABLE `event_types` (
+CREATE TABLE IF NOT EXISTS `event_types` (
   `id` int(11) NOT NULL,
   `value` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -37,7 +37,7 @@ CREATE TABLE `event_types` (
 -- Table structure for table `incentives`
 --
 
-CREATE TABLE `incentives` (
+CREATE TABLE IF NOT EXISTS `incentives` (
   `id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
   `requirement` int(11) NOT NULL,
@@ -47,14 +47,14 @@ CREATE TABLE `incentives` (
   `end_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `incentives`
 --
 
 INSERT INTO `incentives` (`id`, `type`, `requirement`, `comments`, `active`, `start_at`, `end_at`, `created_at`, `updated_at`) VALUES
-(2, 2, 1, 'Just for grad', 0, '2015-07-23 12:00:00', '2015-07-23 12:00:00', '2016-06-22 05:57:31', '2016-06-22 06:00:20');
+(2, 1, 1, 'Just for grad', 0, '2015-07-23 12:00:00', '2015-07-23 12:00:00', '2016-06-22 05:57:31', '2016-06-24 06:13:56');
 
 -- --------------------------------------------------------
 
@@ -62,10 +62,17 @@ INSERT INTO `incentives` (`id`, `type`, `requirement`, `comments`, `active`, `st
 -- Table structure for table `incentive_types`
 --
 
-CREATE TABLE `incentive_types` (
+CREATE TABLE IF NOT EXISTS `incentive_types` (
   `id` int(11) NOT NULL,
   `value` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `incentive_types`
+--
+
+INSERT INTO `incentive_types` (`id`, `value`) VALUES
+(1, 'Efectivo');
 
 -- --------------------------------------------------------
 
@@ -73,10 +80,10 @@ CREATE TABLE `incentive_types` (
 -- Table structure for table `newsletter_types`
 --
 
-CREATE TABLE `newsletter_types` (
+CREATE TABLE IF NOT EXISTS `newsletter_types` (
   `id` int(11) NOT NULL,
   `value` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -84,7 +91,7 @@ CREATE TABLE `newsletter_types` (
 -- Table structure for table `requirement_types`
 --
 
-CREATE TABLE `requirement_types` (
+CREATE TABLE IF NOT EXISTS `requirement_types` (
   `id` int(11) NOT NULL,
   `value` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -95,13 +102,13 @@ CREATE TABLE `requirement_types` (
 -- Table structure for table `surveys`
 --
 
-CREATE TABLE `surveys` (
+CREATE TABLE IF NOT EXISTS `surveys` (
   `id` int(11) NOT NULL,
   `name` varchar(256) NOT NULL,
   `active` tinyint(1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `surveys`
@@ -116,7 +123,7 @@ INSERT INTO `surveys` (`id`, `name`, `active`, `created_at`, `updated_at`) VALUE
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `user` varchar(128) NOT NULL,
   `role` int(11) NOT NULL,
@@ -125,7 +132,7 @@ CREATE TABLE `users` (
   `password` varchar(128) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -149,7 +156,8 @@ ALTER TABLE `event_types`
 -- Indexes for table `incentives`
 --
 ALTER TABLE `incentives`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`type`);
 
 --
 -- Indexes for table `incentive_types`
@@ -195,17 +203,17 @@ ALTER TABLE `event_types`
 -- AUTO_INCREMENT for table `incentives`
 --
 ALTER TABLE `incentives`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `incentive_types`
 --
 ALTER TABLE `incentive_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `newsletter_types`
 --
 ALTER TABLE `newsletter_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `requirement_types`
 --
@@ -215,12 +223,22 @@ ALTER TABLE `requirement_types`
 -- AUTO_INCREMENT for table `surveys`
 --
 ALTER TABLE `surveys`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `incentives`
+--
+ALTER TABLE `incentives`
+  ADD CONSTRAINT `incentive_type` FOREIGN KEY (`type`) REFERENCES `incentive_types` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
