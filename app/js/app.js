2ccdca92dac708e61aef8c3aa422067b259aed7e
Antonio <antonio.hdez93@gmail.com>
2016-06-26 02:04:50 -0500

    Angular router

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,35 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+//Define an angular module for our app
+var app = angular.module('udgSurveys', ['ngRoute']);
+
+//Define Routing for app
+app.config(function($routeProvider) {
+    $routeProvider.
+      when('/', {
+		template: 'Work in progress'
+	}).
+      when('/surveys', {
+		templateUrl: 'templates/surveys.html',
+		controller: 'surveysController'
+	}).
+      when('/surveys/create', {
+		templateUrl: 'templates/surveys/create.html',
+		controller: 'surveysController'
+	}).
+      when('/incentives', {
+		templateUrl: 'templates/incentives.html',
+		controller: 'surveysController'
+	}).
+      when('/users', {
+		templateUrl: 'templates/users.html',
+		controller: 'surveysController'
+	}).
+      otherwise({
+      	template: 'Testing default route'
+		//redirectTo: '/404'
+      });
+});
+
+
+app.controller('surveysController', function($scope) {
+	
+});
